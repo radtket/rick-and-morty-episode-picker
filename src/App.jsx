a5282aa43d152f703e9a59e2d5c0c01f9f4f6471
Taylor Radtke <taylorradtke@gmail.com>
2019-03-26 17:31:01 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<div>
+				<h1>Rick and Morty</h1>
+				<p>Pick your favourite episodes</p>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default App;
